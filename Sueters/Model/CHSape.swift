710280a3e5e4c9dcf296ddfcd1fa5f6799272590
parisollie <pjff18@gmail.com>
2025-04-06 20:06:51 -0600

    MVM

---
+++ Sueters/Model/CHSape.swift
@@ -0,0 +1,22 @@
+//
+//  CHSape.swift
+//  Sueters
+//
+//  Created by Paul F on 06/04/25.
+//
+
+import SwiftUI
+
+// custom bottom corner shape...
+
+struct CHSape : Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.bottomLeft,.bottomRight], cornerRadii: CGSize(width: UIScreen.main.bounds.height < 750 ? 30 : 45, height: UIScreen.main.bounds.height < 750 ? 30 : 45))
+        
+        return Path(path.cgPath)
+    }
+}
+
+var sizes = ["S","M","L","X","XX","XL","XXL"]
